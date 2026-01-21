74ffc5027d9c9e2b6ed28ece6a098adf889e0172
moty629 <motymoty629@gmail.com>
2026-01-21 16:51:04 +0530

    Update build-deb.sh

--- packaging/build-deb.sh
+++ packaging/build-deb.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
-echo "Debian package build placeholder"
+set -e
 
+PKG="tcall"
+VER="0.1"
+ARCH="all"
+BUILD="/tmp/${PKG}_${VER}"
+OUT="${PKG}_${VER}_${ARCH}.deb"
+
+rm -rf "$BUILD"
+
+mkdir -p "$BUILD/DEBIAN"
+mkdir -p "$BUILD/usr/local/bin"
+mkdir -p "$BUILD/etc/systemd/system"
+
+cp debian/control "$BUILD/DEBIAN/control"
+cp debian/postinst "$BUILD/DEBIAN/postinst"
+cp debian/prerm "$BUILD/DEBIAN/prerm"
+
+chmod 755 "$BUILD/DEBIAN/postinst" "$BUILD/DEBIAN/prerm"
+
+cp bin/* "$BUILD/usr/local/bin/"
+chmod 755 "$BUILD/usr/local/bin/"*
+
+cp systemd/tcall.service "$BUILD/etc/systemd/system/"
+
+dpkg-deb --build "$BUILD" "$OUT"
+
+echo "Package built: $OUT"
